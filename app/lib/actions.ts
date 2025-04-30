b419b648c40631292964800f5fbf4779b5f07937
neaL367 <atichatfantasy@gmail.com>
2025-05-01 03:32:22 +0700

    fix: type error on prod

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -66,6 +66,7 @@ export async function createInvoice(prevState: State, formData: FormData) {
       `;
     } catch (error) {
         // If a database error occurs, return a more specific error.
+        console.error('Database error:', error);
         return {
             message: 'Database Error: Failed to Create Invoice.',
         };
@@ -104,6 +105,7 @@ export async function updateInvoice(
         WHERE id = ${id}
       `;
     } catch (error) {
+        console.error('Database error:', error);
         return { message: 'Database Error: Failed to Update Invoice.' };
     }
 
